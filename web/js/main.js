509d925e2ddd736b88241002c9fffd3d17c7fb40
Ronan Guilloux <ronan.guilloux@gmail.com>
2013-12-11 23:40:18 +0000

    easing navigation & improving GA stats

--- web/js/main.js
+++ web/js/main.js
@@ -1,4 +1,12 @@
-$('ul.nav li a').click(function(el,ev){
+$('ul.nav li a').click(function(ev){
+    ev.preventDefault();
     $('ul.nav li').removeClass('active');
-    $(this).parent().addClass('active');
+    var $anchor = $(this);
+    $anchor.parent().addClass('active');
+    $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top
+    }, 1500,'easeInOutExpo', function() {
+        window.location.hash = $anchor.attr('href').substr(1);
+    });
+    _gaq.push(['_trackPageview',location.pathname + location.search  + location.hash]);
 });

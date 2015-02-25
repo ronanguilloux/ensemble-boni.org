d190c85635446d44858da02f194a9ea7de5d5a6e
Ronan Guilloux <ronan.guilloux@gmail.com>
2015-02-25 01:44:21 +0100

    improving NodeJs local server

--- server.js
+++ server.js
@@ -4,8 +4,11 @@
  * Distributed under terms of the MIT license.
  */
 
-// $ npm install connect
-var connect = require('connect');
-connect().use(connect.static(__dirname)).listen(8081);
-// $ node server.js
+// npm install connect serve-static
+// node server.js
 // then: http://localhost:8081/
+//
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(8081);
+console.log("open http://localhost:8081/index.html");
